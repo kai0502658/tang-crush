be91298e7f907f85c0b28ec00fc4f1ba69d58731
kai0502658 <85253285+kai0502658@users.noreply.github.com>
2021-06-06 14:23:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     Oppa thương hấp lắm á
     Anh biết hấp cũng thương anh mà
     Đúng k`,
-    btnIntro: 'Vâng',
+    btnIntro: 'Rồi sao',
     title: 'Hấp tha lỗi cho oppa nhá',
     desc: 'Nháaaaaaaaaaaaaaaa ',
     btnYes: 'Dạ, Bé còi k giận a đâu ạ <33',
@@ -12,7 +12,7 @@ const CONFIG = {
     question:'Hí hí hí, Yêu em nhất. Anh biết hấp sẽ bỏ qua cho a mà.hihi <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Em cũng yêu anh nhất hihi <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Anh biết mà. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Gọi lại ngay cho anh nhé, a đang đợi em call đấy.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
